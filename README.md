4388d14f3131c900232ecc6644a487fc7a0ec913
ianband <ian.r.band@gmail.com>
2022-01-20 02:01:01 -0800

    coming along lol

--- README.md
+++ README.md
@@ -2,10 +2,8 @@ generate random valid js
 
 idk i'm bored lol
 
-
 - https://babeljs.io/docs/en/babel-types, why is this so hard to find on google?
 - https://astexplorer.net/
-
 
 need this: 
 
--- babel-plugins/monkey.js
+++ babel-plugins/monkey.js
@@ -6,12 +6,10 @@ module.exports = function infiniteJSMonkey({types: t}) {
             Program(path, state){
 
                 console.log('monkey!');
-                console.log(defs.NODE_FIELDS.ObjectPattern.typeAnnotation.validate.oneOfNodeTypes);
-                console.log(defs.NODE_FIELDS.BindExpression.object.validate.oneOfNodeTypes);
-                //console.log(defs.NODE_FIELDS.ObjectPattern.typeAnnotation.validate);
+                console.log(defs.NODE_FIELDS.ObjectExpression);
+                console.log(randomChildNodes("ObjectExpression"));
 
-                //console.log(Object.keys(t).filter(key => key == 'nullLiteral'));
-                // we start by declaring a single js variable
+
                 let rootVarName = "us_toast";
                 let toast = t.variableDeclaration(
                     "let", 
@@ -19,12 +17,43 @@ module.exports = function infiniteJSMonkey({types: t}) {
                 );
                 path.node.body = [toast];
 
+                /*
+                let testNode = t.expressionStatement(
+                    t.assignmentExpression(
+                        "=",
+                        t.identifier(rootVarName),
+
+                    )
+                )
+                */
+
                 // now we can add more lines to the program by pushing them onto path.node.body
                 // path.node.body.push();
-
-
-
             }
         }
     };
+}
+
+function randomChildNodes(parentNodeType){
+
+    // loop through parent node's possible childeren
+        // select random valid node for that child
+
+
+    return Object.keys(defs.NODE_FIELDS[parentNodeType]).map(
+        child => {
+
+            // if child has a default value, do we use it? 50% chance
+            // if child is optional, do we include it? 50% chance
+            // if child is chainOf (an array of) nodes, how many nodes should we have? random n in [1..10]
+            // if child is oneOfNodeTypes, which node type do we choose? a random one
+
+            // ObjectPattern seems to be a good test case?
+            //if(defs.NODE_FIELDS[parentNodeType][child].default)
+                return defs.NODE_FIELDS[parentNodeType][child].default; // for now
+
+            //if(defs.NODE_FIELDS[
+            //return;
+        }
+    )
 }
